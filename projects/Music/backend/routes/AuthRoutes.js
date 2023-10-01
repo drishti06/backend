1b6919bfc60d527a62587ad83ce824bf597c3b77
unknown <kurmavanhi81@gmail.com>
2023-10-01 10:54:29 +0530

    new changes

--- projects/Music/backend/routes/AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { userLogin, userRegister } = require('../controller/AuthController.js');
-
-router.use('/login', userLogin);
-router.use('/register', userRegister);
-
-module.exports = router;
